78e209d105a0fe85d7ba1e870c1916afb5f761be
tolgatunaaa <tolga.tuna@digital.hmrc.gov.uk>
2022-10-10 23:23:55 +0100

    ITS-1370 - Versions increased

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,9 +5,9 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.22.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.24.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.11.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "6.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.3.0",
     "uk.gov.hmrc"       %% "play-language"                  % "5.3.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0"
   )

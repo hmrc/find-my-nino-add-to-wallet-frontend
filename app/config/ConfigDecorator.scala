27375f82bfbf49ba13c153770d2cfcc2174372af
rishisachania <rishisachania@gmail.com>
2023-02-28 09:01:11 +0000

    fixes for signout, session and feedback services

--- app/config/ConfigDecorator.scala
+++ app/config/ConfigDecorator.scala
@@ -17,11 +17,13 @@
 package config
 
 import com.google.inject.{Inject, Singleton}
+import controllers.bindable.Origin
 import play.api.Configuration
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
 import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import java.net.URLEncoder
 
 @Singleton
 class ConfigDecorator @Inject()(configuration: Configuration, servicesConfig: ServicesConfig) {
@@ -34,14 +36,32 @@ class ConfigDecorator @Inject()(configuration: Configuration, servicesConfig: Se
 
   val gtmContainer: String = configuration.get[String]("tracking-consent-frontend.gtm.container")
 
+  val enc = URLEncoder.encode(_: String, "UTF-8")
+
+  private def getExternalUrl(key: String): Option[String] =
+    configuration.getOptional[String](s"external-url.$key")
+
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$serviceName&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
+
+  def getFeedbackSurveyUrl(origin: Origin): String =
+    feedbackSurveyFrontendHost + "/feedback/" + enc(origin.origin)
+
+  def betaFeedbackUnauthenticatedUrl(aDeskproToken: String): String =
+    s"$contactHost/contact/beta-feedback-unauthenticated?service=$aDeskproToken"
+
 
   val loginUrl: String                  = configuration.get[String]("urls.login")
   val loginContinueUrl: String          = configuration.get[String]("urls.loginContinue")
   val signOutUrl: String                = configuration.get[String]("urls.signOut")
   lazy val findMyNinoServiceUrl: String = servicesConfig.baseUrl("find-my-nino-add-to-wallet-service")
   lazy val citizenDetailsServiceUrl: String = servicesConfig.baseUrl("citizen-details-service")
+
+  lazy val basGatewayFrontendHost = getExternalUrl(s"bas-gateway-frontend.host").getOrElse("")
+  lazy val taxEnrolmentAssignmentFrontendHost = getExternalUrl(s"tax-enrolment-assignment-frontend.host").getOrElse("")
+  lazy val pertaxFrontendHost = getExternalUrl(s"pertax-frontend.host").getOrElse("")
+  lazy val pertaxFrontendForAuthHost = getExternalUrl(s"pertax-frontend.auth-host").getOrElse("")
+  lazy val feedbackSurveyFrontendHost = getExternalUrl(s"feedback-survey-frontend.host").getOrElse("")
 
 
   private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
---
+++ app/controllers/PublicController.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import config.ConfigDecorator
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.auth.core.AuthConnector
+import views.html.public.SessionTimeoutView
+import scala.concurrent.{ExecutionContext, Future}
+
+class PublicController @Inject()(sessionTimeoutView: SessionTimeoutView,authConnector: AuthConnector)(implicit
+                                                                                                           configDecorator: ConfigDecorator,
+                                                                                                           ec: ExecutionContext,
+                                                                                                           cc: MessagesControllerComponents,
+                                                                                                           config: Configuration,
+                                                                                                           env: Environment
+) extends FMNBaseController(authConnector) with I18nSupport  {
+
+
+
+  def sessionTimeout: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful {
+      Ok(sessionTimeoutView())
+    }
+  }
+
+  def redirectToYourProfile(): Action[AnyContent] = Action.async { _ =>
+    Future.successful {
+      Ok("")
+      //Redirect(controllers.address.routes.PersonalDetailsController.onPageLoad)
+    }
+  }
+
+}

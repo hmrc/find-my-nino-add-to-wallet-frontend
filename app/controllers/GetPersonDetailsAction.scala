56e7eb485e075bcf4af173804e0094765c22d8e4
rishisachania <rishisachania@gmail.com>
2023-05-20 00:15:08 +0100

    Adding logic redirect to postal form when citizen details returns 404

--- app/controllers/GetPersonDetailsAction.scala
+++ app/controllers/GetPersonDetailsAction.scala
@@ -18,65 +18,66 @@ package controllers
 
 import com.google.inject.Inject
 import config.ConfigDecorator
-import connectors.{CitizenDetailsConnector, PersonDetailsHiddenResponse, PersonDetailsSuccessResponse}
+import connectors.{CitizenDetailsConnector, PersonDetailsHiddenResponse, PersonDetailsNotFoundResponse, PersonDetailsSuccessResponse}
 import controllers.auth.AuthContext
 import controllers.auth.requests.UserRequest
 import models.{PersonDetails, UserName}
-import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.Results.Locked
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.Results.{Locked, NotFound}
 import play.api.mvc._
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
-import views.html.ManualCorrespondenceView
+import views.html.RedirectToPostalFormView
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
-
-class GetPersonDetailsAction @Inject() (
-                                         citizenDetailsConnector: CitizenDetailsConnector,
-                                         cc: ControllerComponents,
-                                         val messagesApi: MessagesApi,
-                                         manualCorrespondenceView: ManualCorrespondenceView
-                                       )(implicit configDecorator: ConfigDecorator, ec: ExecutionContext)
+class GetPersonDetailsAction @Inject()(
+                                        citizenDetailsConnector: CitizenDetailsConnector,
+                                        cc: ControllerComponents,
+                                        val messagesApi: MessagesApi,
+                                        redirectView: RedirectToPostalFormView
+                                      )(implicit configDecorator: ConfigDecorator, ec: ExecutionContext)
   extends ActionRefiner[AuthContext, UserRequest]
     with ActionFunction[AuthContext, UserRequest]
     with I18nSupport {
 
   override protected def refine[A](authContext: AuthContext[A]): Future[Either[Result, UserRequest[A]]] = {
 
-   getPersonDetails(authContext).map {
-        case Left(error) => Left(error)
-        case Right(pd)   =>
-          Right(
-            UserRequest(
-              Some(Nino(authContext.nino.nino)),
-              Some(UserName(authContext.name)),
-              authContext.confidenceLevel,
-              pd,
-              authContext.allEnrolments,
-              authContext.request
-            )
+    getPersonDetails(authContext).map {
+      case Left(error) => Left(error)
+      case Right(personDetails) =>
+        Right(
+          UserRequest(
+            Some(Nino(authContext.nino.nino)),
+            Some(UserName(authContext.name)),
+            authContext.confidenceLevel,
+            personDetails,
+            authContext.allEnrolments,
+            authContext.request
           )
-      }
+        )
     }
+  }
 
   private def getPersonDetails(authContext: AuthContext[_]): Future[Either[Result, Option[PersonDetails]]] = {
 
     implicit val hc: HeaderCarrier =
       HeaderCarrierConverter.fromRequestAndSession(authContext.request, authContext.request.session)
 
+    implicit val messages: Messages = cc.messagesApi.preferred(authContext.request)
     authContext.nino.nino match {
       case nino =>
         citizenDetailsConnector.personDetails(Nino(nino)).map {
-          case PersonDetailsSuccessResponse(pd) =>
-            Right(Some(pd))
-          case PersonDetailsHiddenResponse      =>
-            Left(Locked("Person details are locked"))
-          case _                                => Right(None)
+          case PersonDetailsSuccessResponse(pd) => Right(Some(pd))
+          case PersonDetailsNotFoundResponse =>
+            Left(NotFound(redirectView()(authContext.request, configDecorator, messages)))
+          case PersonDetailsHiddenResponse =>
+            Left(Locked(redirectView()(authContext.request, configDecorator, messages)))
+          case _ => Right(None)
         }
-      case _          => Future.successful(Right(None))
+      case _ => Future.successful(Right(None))
     }
   }
 

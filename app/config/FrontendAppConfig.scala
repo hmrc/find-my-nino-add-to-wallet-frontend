b8b3e0e75c6ea6157f8c3bdbbe5bb0761dbe9a57
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2023-06-16 16:21:40 +0100

    ITS-2731: Uplift journey changes.

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -46,7 +46,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   val signOutUrl: String                = configuration.get[String]("urls.signOut")
   lazy val findMyNinoServiceUrl: String = servicesConfig.baseUrl("find-my-nino-add-to-wallet-service")
 
-  lazy val pertaxFrontendHost: String = getExternalUrl("pertax-frontend.host").getOrElse("")
+  lazy val pertaxFrontendAuthHost: String = getExternalUrl("pertax-frontend.auth-host").getOrElse("")
 
   private val exitSurveyBaseUrl: String = getExternalUrl(s"feedback-survey-frontend.host").getOrElse("")
   val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/save-your-national-insurance-number"

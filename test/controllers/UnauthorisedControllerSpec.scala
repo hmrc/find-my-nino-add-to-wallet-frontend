54e2f7686f67f52dea623ece7250f8f91e69c474
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2023-07-19 13:55:06 +0100

    Compile fixes

--- test/controllers/UnauthorisedControllerSpec.scala
+++ test/controllers/UnauthorisedControllerSpec.scala
@@ -25,7 +25,6 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import repositories.SessionRepository
 import uk.gov.hmrc.sca.connectors.ScaWrapperDataConnector
-import util.Keys
 import views.html.UnauthorisedView
 
 import scala.concurrent.Future

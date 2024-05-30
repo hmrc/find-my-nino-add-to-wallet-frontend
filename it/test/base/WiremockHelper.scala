2c9dd0321690d36d4f7b391ff930ffb067649c0a
Shihabur Rahman <98172596+Shihabur-Atos@users.noreply.github.com>
2024-05-30 11:41:18 +0100

    ITS-3454 | Implement SMN FE play 3.0 upgrade (#189)

--- it/test/base/WiremockHelper.scala
+++ it/test/base/WiremockHelper.scala
@@ -16,8 +16,8 @@
 
 package base
 
-import akka.actor.ActorSystem
-import akka.stream.Materializer
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.Materializer
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock._

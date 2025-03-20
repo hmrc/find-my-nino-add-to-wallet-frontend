a00ed45c1c4291a3d492e66076992adce5e914bb
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2025-03-20 13:31:53 +0000

    DDCNL-9276: Updated spec

--- it/test/connectors/AppleWalletConnectorSpec.scala
+++ it/test/connectors/AppleWalletConnectorSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,9 @@ import config.FrontendAppConfig
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.Application
 import play.api.libs.json.{Json, Writes}
-import play.api.mvc.Results.InternalServerError
 import play.api.test.{DefaultAwaitTimeout, Injecting}
-import uk.gov.hmrc.http.HttpClient
+import uk.gov.hmrc.http.UpstreamErrorResponse
+import uk.gov.hmrc.http.client.HttpClientV2
 import util.WireMockHelper
 
 import java.util.Base64
@@ -36,32 +36,28 @@ class AppleWalletConnectorSpec extends ConnectorSpec
   implicit val appleWrites: Writes[ApplePassDetails] = Json.writes[ApplePassDetails]
 
   override implicit lazy val app: Application = app(
-    Map("microservice.services.find-my-nino-add-to-wallet-service.port" -> server.port())
+    Map("microservice.services.find-my-nino-add-to-wallet-service.port" -> server.port(),
+        "microservice.services.find-my-nino-add-to-wallet-service.host" -> "127.0.0.1")
   )
 
   val delay = 5000
 
   val passId: String = "passId"
   val applePassCardBytes: Array[Byte] = Array(99, 71, 86, 121, 99, 50, 57, 117, 82, 71, 86, 48, 89, 87, 108, 115, 99, 49, 78, 48, 99, 109, 108, 117, 90, 119, 61, 61)
-  val applePassCard = Base64.getEncoder.encodeToString(applePassCardBytes)
+  val applePassCard: String = Base64.getEncoder.encodeToString(applePassCardBytes)
   val fakeName: String = "fakeName"
   val fakeNino:String = "fakeNino"
-  val createApplePassDetails = ApplePassDetails(fakeName, fakeNino)
-
-  val errMsg = Json.obj(
-    "status" -> "500",
-    "message" -> "someMsg"
-  )
-
+  val createApplePassDetails: ApplePassDetails = ApplePassDetails(fakeName, fakeNino)
 
   trait SpecSetup {
 
     def url: String
 
-    lazy val connector = {
-      val httpClient = app.injector.instanceOf[HttpClient]
+    lazy val connector: AppleWalletConnector = {
+      val httpClientV2 = app.injector.instanceOf[HttpClientV2]
       val frontendAppConfig = app.injector.instanceOf[FrontendAppConfig]
-      new AppleWalletConnector(frontendAppConfig, httpClient)
+      val httpClientResponse = app.injector.instanceOf[HttpClientResponse]
+      new AppleWalletConnector(frontendAppConfig, httpClientV2, httpClientResponse)
     }
   }
 
@@ -73,15 +69,26 @@ class AppleWalletConnectorSpec extends ConnectorSpec
 
     "return Pass when called with an existing pass Id" in new LocalSetup {
       stubGet(url, OK, Some(applePassCard))
-      val result = connector.getApplePass(passId).futureValue.get
-      result mustBe applePassCardBytes
+      val result = connector.getApplePass(passId).value.futureValue
+
+      result mustBe a[Right[_, _]]
+      result.getOrElse(None).get must contain theSameElementsAs applePassCardBytes
     }
 
-    "return empty Array when called with an unknown passId" in new LocalSetup {
-      stubGet(url, OK, None)
-      val result = connector.getApplePass(passId).futureValue.get
-      result mustBe Array()
+    "return None when called with an unknown passId" in new LocalSetup {
+      stubGet(url, NOT_FOUND, None)
+      val result = connector.getApplePass(passId).value.futureValue
 
+      result mustBe a[Right[_, _]]
+      result mustBe Right(None)
+    }
+
+    "return Left(UpstreamErrorResponse) when API call fails" in new LocalSetup {
+      stubGet(url, INTERNAL_SERVER_ERROR, None)
+      val result = connector.getApplePass(passId).value.futureValue
+
+      result mustBe a[Left[UpstreamErrorResponse, _]]
+      result.swap.getOrElse(UpstreamErrorResponse("", OK)).statusCode mustBe INTERNAL_SERVER_ERROR
     }
   }
 
@@ -91,17 +98,28 @@ class AppleWalletConnectorSpec extends ConnectorSpec
       def url: String = s"/find-my-nino-add-to-wallet/get-qr-code?passId=$passId"
     }
 
-    "return Pass when called with an existing pass Id" in new LocalSetup {
+    "return QR code when called with an existing pass Id" in new LocalSetup {
       stubGet(url, OK, Some(applePassCard))
-      val result = connector.getAppleQrCode(passId).futureValue.get
-      result mustBe applePassCardBytes
+      val result = connector.getAppleQrCode(passId).value.futureValue
+
+      result mustBe a[Right[_, _]]
+      result.getOrElse(None).get must contain theSameElementsAs applePassCardBytes
     }
 
-    "return empty Array when called with an unknown passId" in new LocalSetup {
-      stubGet(url, OK, None)
-      val result = connector.getAppleQrCode(passId).futureValue.get
-      result mustBe Array()
+    "return None when called with an unknown passId" in new LocalSetup {
+      stubGet(url, NOT_FOUND, None)
+      val result = connector.getAppleQrCode(passId).value.futureValue
 
+      result mustBe a[Right[_, _]]
+      result mustBe Right(None)
+    }
+
+    "return Left(UpstreamErrorResponse) when API call fails" in new LocalSetup {
+      stubGet(url, INTERNAL_SERVER_ERROR, None)
+      val result = connector.getAppleQrCode(passId).value.futureValue
+
+      result mustBe a[Left[UpstreamErrorResponse, _]]
+      result.swap.getOrElse(UpstreamErrorResponse("", OK)).statusCode mustBe INTERNAL_SERVER_ERROR
     }
   }
 
@@ -111,17 +129,20 @@ class AppleWalletConnectorSpec extends ConnectorSpec
       def url: String = s"/find-my-nino-add-to-wallet/create-apple-pass"
     }
 
-    "return OK when called create pass" in new LocalSetup {
+    "return Some(passId) when called create pass" in new LocalSetup {
       stubPost(url, OK, Some(Json.toJson(createApplePassDetails).toString()), Some(passId))
-      val result = connector.createApplePass(createApplePassDetails.fullName,createApplePassDetails.nino).futureValue.get
-      result mustBe passId
+      val result = connector.createApplePass(createApplePassDetails.fullName, createApplePassDetails.nino).value.futureValue
+
+      result mustBe a[Right[_, _]]
+      result.getOrElse(None) mustBe Some(passId)
     }
 
-    "return error when called create pass " in new LocalSetup {
+    "return Left(UpstreamErrorResponse) when API call fails" in new LocalSetup {
       stubWithDelay(url, INTERNAL_SERVER_ERROR, Some(Json.toJson(createApplePassDetails).toString()), None, delay)
-      val result = connector.createApplePass(createApplePassDetails.fullName,createApplePassDetails.nino)
-        .value.getOrElse(InternalServerError(Json.toJson(errMsg)))
-      result mustBe InternalServerError(Json.toJson(errMsg))
+      val result = connector.createApplePass(createApplePassDetails.fullName, createApplePassDetails.nino).value.futureValue
+
+      result mustBe a[Left[UpstreamErrorResponse, _]]
+      result.swap.getOrElse(UpstreamErrorResponse("", OK)).statusCode mustBe INTERNAL_SERVER_ERROR
     }
   }
 }

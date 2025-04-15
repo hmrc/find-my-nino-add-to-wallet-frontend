b268e8cf0324bc56ad26a64a41f8a84e7f6ecd68
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2025-04-15 15:20:16 +0100

    DDCNL-9280: pr-bot comment fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,11 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %% s"play-conditional-form-mapping-$playVersion"        %   "3.2.0",
+    "uk.gov.hmrc"                   %% s"play-conditional-form-mapping-$playVersion"        %   "3.3.0",
     "uk.gov.hmrc.mongo"             %% s"hmrc-mongo-$playVersion"                           %   mongoVersion,
     "org.apache.xmlgraphics"        % "fop"                                                 %   "2.10",
     "org.typelevel"                 %% "cats-core"                                          %   "2.10.0",
-    "uk.gov.hmrc"                   %% s"sca-wrapper-$playVersion"                          %   "2.7.0",
+    "uk.gov.hmrc"                   %% s"sca-wrapper-$playVersion"                          %   "2.9.0",
     "com.google.auth"               % "google-auth-library-oauth2-http"                     %   "1.23.0",
     "com.auth0"                     % "java-jwt"                                            %   "4.4.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"                               %   "2.17.0",
